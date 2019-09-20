384774e056cfdb11224762f305525a3b17aa519c
tuh8888 <harrison.pielke-lombardo@cuanschutz.edu>
2019-09-20 16:59:01 -0600

    Show my website code

--- src/hpl_website/about.clj
+++ src/hpl_website/about.clj
@@ -6,34 +6,32 @@
 
 (def page-title "About")
 
+(def tools {"Ring"         "https://github.com/ring-clojure/ring"
+            "Compojure"    "https://github.com/weavejester/compojure"
+            "Hiccup"       "https://github.com/weavejester/hiccup"
+            "Timbre"       "https://github.com/ptaoussanis/timbre"
+            "markdown-clj" "https://github.com/yogthos/markdown-clj"})
+
+(def code {:owner "tuh8888"
+           :repo  "hpl-website"
+           :ref   "master"
+           :embed [{:path "README.md"}
+                   {:path "src/hpl_website/core.clj"}
+                   {:path "src/hpl_website/about.clj"}]})
+
 (compojure/defroutes about-routes
   (compojure/context "/about" []
     (compojure/GET "/" []
       (util/hpl-page page-title
         [:div
-         [:p "The code for this site can be found "
-          (el/link-to "https://github.com/tuh8888/hpl-website" "here"
-                      ".")]
+         [:p "The code for this site is shown below."]
          [:p "Built entirely in Clojure. Tools/packages used:"]
-         (el/unordered-list
-           (util/linkify
-             {"Ring"         "https://github.com/ring-clojure/ring"
-              "Compojure"    "https://github.com/weavejester/compojure"
-              "Hiccup"       "https://github.com/weavejester/hiccup"
-              "Timbre"       "https://github.com/ptaoussanis/timbre"
-              "markdown-clj" "https://github.com/yogthos/markdown-clj"}))
-         [:div#settings-object
-          (util/style :height "500px"
-                      :margin-bottom "2rem")]
+         (el/unordered-list (util/linkify tools))
+         [:div#settings-object (util/style :height "500px"
+                                           :margin-bottom "2rem")]
          [:script {:src "https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.23.0/polyfill.min.js"}]
          [:script {:src "/js/github-embed.min.js"}]
-         (let [settings {"owner" "tuh8888"
-                         "repo"  "hpl-website"
-                         "ref"   "master"
-                         "embed" [{:path "src/hpl_website/index.clj"}
-                                  {:path "README.md"}]}]
-           [:script (format "githubEmbed('#settings-object', %s);"
-                            (json/write-str settings))])]))
+         [:script (format "githubEmbed('#settings-object', %s);" (json/write-str code))]]))
 
     (compojure/GET "/research" []
       (util/hpl-page page-title
