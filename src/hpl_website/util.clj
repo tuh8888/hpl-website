0446c4fe508bc824565535c63f788ebe53c4fe1e
tuh8888 <harrison.pielke-lombardo@cuanschutz.edu>
2019-09-20 12:09:32 -0600

    Dropdown menu

--- src/hpl_website/util.clj
+++ src/hpl_website/util.clj
@@ -1,7 +1,8 @@
 (ns hpl-website.util
   (:require [hiccup.page :as page]
             [clojure.java.io :as io]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+            [hiccup.element :as el]))
 
 (def my-info (-> "my-info.edn"
                  (io/resource)
@@ -11,11 +12,22 @@
 (def sub-page "%s | %s")
 (def site-title (:name my-info))
 
+(defn nav-drop-down
+  "Dropdown menu"
+  [parent-url parent-content children]
+  [:div.dropdown
+   [:span (el/link-to parent-url parent-content)]
+   (->> children
+        (map (fn [[url content]]
+               (el/link-to (format "%s/%s" parent-url url)
+                           content)))
+        (into [:div.dropdown-content]))])
+
 (def nav
-  [:ul
-   [:li [:a {:href "index"} "Home"]]
-   [:li [:a {:href "about"} "About"]]
-   [:li [:a {:href "contact"} "Contact"]]])
+  (el/unordered-list [(el/link-to "/index" "Home")
+                      (nav-drop-down "/about" "About" {"research" "Research"
+                                                       "music"    "Music"})
+                      (el/link-to "/contact" "Contact")]))
 
 (defn hpl-page
   [page-title content]
@@ -23,7 +35,7 @@
               [:head [:title (if page-title
                                (format sub-page site-title page-title)
                                site-title)]
-               (page/include-css "css/index.css")]
+               (page/include-css "/css/index.css")]
               [:body
                [:div [:h1 site-title]]
                [:div#nav nav]
